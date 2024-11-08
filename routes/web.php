3338941e8bebdb2e5bcde78333a05faff4b06c79
Luuk4mst3l <l.e.amstel@gmail.com>
2024-11-08 12:15:09 +0100

    lost it

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return respone('h1');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
